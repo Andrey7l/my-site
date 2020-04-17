75106d333ed22d38602dcda4280180481a44c8b8
Andrey Lyah <Andrey7l5c@gmail.com>
2020-04-17 21:47:03 +0300

    ccc

--- src/apps/index/models.py
+++ src/apps/index/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from django.db import models as m
 
-# Create your models here.
+
+# какие колонки с какой таблицы читать - (Model)
+# булем испольовать постгиэс (postgies)
+class UserInfo(m.Model):
+    name = m.TextField(unique=True)
+    greeting = m.TextField(null=True, blank=True)
+    age = m.IntegerField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "User Info"  # выставл прав имя а не джанго дичку
+
+        def __str__(self):
+            return f"UserInfo(id={self.pk},name={self.name!r})"
+
+
+
+
+#a = UserInfo()  - где 'а' это объект
+#b = UserInfo()
+#print(a.name)
+#print(b.name)
+
